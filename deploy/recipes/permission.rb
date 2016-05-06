ca678fa33e396683f65fd306feb920a2b8fd9323
Naoki Iha <sabohoney.com@gmail.com>
2016-05-06 17:17:14 +0900

    Add Deploy Recipe.

---
+++ deploy/recipes/permission.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: deploy
+# Recipe:: basercms-deploy
+#
+
+node[:deploy].each do |app_name, deploy|
+
+  # Add write-access permission to "shared/log" directory.
+  execute "Add write-access permission to storage directory" do
+    command "chown -R apache:#{deploy[:group]} #{deploy[:deploy_to]}/current"
+  end
+
+  # Add write-access permission to "shared/log" directory.
+  execute "Add write-access permission to storage directory" do
+    command "chmod -R g+w #{deploy[:deploy_to]}/current"
+  end
+
+end
