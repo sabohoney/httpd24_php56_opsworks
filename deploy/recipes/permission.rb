19af40f8029bdc5fb9942d0be1bf0f0e42d450f8
Naoki Iha <sabohoney.com@gmail.com>
2016-05-07 22:36:30 +0900

    fixed syntax.

--- deploy/recipes/permission.rb
+++ deploy/recipes/permission.rb
@@ -5,14 +5,16 @@
 
 node[:deploy].each do |app_name, deploy|
 
-  # Add write-access permission to "shared/log" directory.
-  execute "Add write-access permission to storage directory" do
-    command "chown -R apache:#{deploy[:group]} #{deploy[:deploy_to]}/current"
-  end
-
-  # Add write-access permission to "shared/log" directory.
-  execute "Add write-access permission to storage directory" do
-    command "chmod -R g+w #{deploy[:deploy_to]}/current"
+  if ::File.exists?("#{deploy[:deploy_to]}/current/app")
+    # Add write-access permission to "shared/log" directory.
+    execute "Add write-access permission to storage directory" do
+      command "chown -R apache:#{deploy[:group]} #{deploy[:deploy_to]}/current"
+    end
+  
+    # Add write-access permission to "shared/log" directory.
+    execute "Add write-access permission to storage directory" do
+      command "chmod -R g+w #{deploy[:deploy_to]}/current"
+    end
   end
 
 end
