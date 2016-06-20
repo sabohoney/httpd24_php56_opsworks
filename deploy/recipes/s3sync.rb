c9ad3184ebd5a421c1b3bbd87b21c60f9c73b5b2
Naoki Iha <n_iha@wan55.co.jp>
2016-06-20 21:03:57 +0900

    change to sync recipe.

--- deploy/recipes/s3sync.rb
+++ deploy/recipes/s3sync.rb
@@ -37,18 +37,9 @@ node[:deploy].each do |app_name, deploy|
   gid = %x(id -g apache)
   execute "export" do
     command <<-EOH
-      goofys #{node[:basercms_deploy][:bucket_name]} #{deploy[:deploy_to]}/current/app/webroot -o allow_other,--uid=4000,--gid=48
+      goofys #{node[:basercms_deploy][:bucket_name]} #{deploy[:deploy_to]}/current/app/webroot -o allow_other,--uid=#{uid},--gid=#{gid}
       sleep 30s
     EOH
-  end
-  # fstab
-  mount "#{deploy[:deploy_to]}/current/app/webroot" do
-    device   "goofys##{node[:basercms_deploy][:bucket_name]}"
-    pass     0
-    dump     0
-    fstype   'fuse'
-    options  "_netdev,allow_other,--uid=4000,--gid=48"
-    action   [:mount, :enable]
   end
   if isCreate == true
     execute "git checkout" do
@@ -64,6 +55,15 @@ node[:deploy].each do |app_name, deploy|
 #     execute "Add write-access permission to storage directory" do
 #       command "chmod -R g+w #{deploy[:deploy_to]}/current/app"
 #     end
+  end
+  # fstab
+  mount "#{deploy[:deploy_to]}/current/app/webroot" do
+    device   "goofys##{node[:basercms_deploy][:bucket_name]}"
+    pass     0
+    dump     0
+    fstype   'fuse'
+    options  "_netdev,allow_other,--uid=#{uid},--gid=#{gid}"
+    action   [:mount, :enable]
   end
 
 end
