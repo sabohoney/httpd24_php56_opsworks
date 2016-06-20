4114e55955b0987876a328e42ccfcfd8062dbb07
Naoki Iha <sabohoney.com@gmail.com>
2016-06-21 05:39:36 +0900

    change to sync recipe.

--- deploy/recipes/s3sync.rb
+++ deploy/recipes/s3sync.rb
@@ -35,7 +35,7 @@ node[:deploy].each do |app_name, deploy|
   # Mount
   uid = '4000'#%x(id -u deploy)
   gid = '48'#%x(id -g apache)
-  execute "export" do
+  execute "s3 Sync by goofys" do
     command <<-EOH
       goofys #{node[:basercms_deploy][:bucket_name]} #{deploy[:deploy_to]}/current/app/webroot -o allow_other,--uid=#{uid},--gid=#{gid}
       sleep 30s
@@ -57,13 +57,13 @@ node[:deploy].each do |app_name, deploy|
 #     end
   end
   # fstab
-#   mount "#{deploy[:deploy_to]}/current/app/webroot" do
-#     device   "goofys##{node[:basercms_deploy][:bucket_name]}"
-#     pass     0
-#     dump     0
-#     fstype   'fuse'
-#     options  "_netdev,allow_other,--uid=#{uid},--gid=#{gid}"
-#     action   [:mount, :enable]
-#   end
+  mount "#{deploy[:deploy_to]}/current/app/webroot" do
+    device   "/opt/go/bin/goofys##{node[:basercms_deploy][:bucket_name]}"
+    pass     0
+    dump     0
+    fstype   'fuse'
+    options  "_netdev,allow_other,--uid=#{uid},--gid=#{gid}"
+    action   [:mount, :enable]
+  end
 
 end
