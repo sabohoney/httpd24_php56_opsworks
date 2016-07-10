f02e6eed12dcd489000e31412f6b7cbfcdbcc4c4
Naoki Iha <sabohoney.com@gmail.com>
2016-07-10 18:18:38 +0900

    Change to phpmyadmin recipe. Change to Version by Berkshelf.

--- deploy/recipes/phpmyadmin-deploy.rb
+++ deploy/recipes/phpmyadmin-deploy.rb
@@ -6,6 +6,12 @@
 node[:deploy].each do |app_name, deploy|
 
   if app_name == "phpmyadmin"
+    # Install
+    composer_project "#{deploy[:deploy_to]}/current/" do
+      dev false
+      quiet true
+      action :install
+    end
     # Setting
     template "#{deploy[:deploy_to]}/current/config.inc.php" do
       group deploy[:group]
