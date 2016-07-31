c20cdb179ea0f4f290dc582956365a406568c1b3
Naoki Iha <sabohoney.com@gmail.com>
2016-08-01 03:03:30 +0900

    fixed syntax error.

--- deploy/recipes/basercms-deploy.rb
+++ deploy/recipes/basercms-deploy.rb
@@ -8,27 +8,25 @@ node[:deploy].each do |app_name, deploy|
   if !deploy[:environment_variables][:type].nil? && deploy[:environment_variables][:type] == "basercms"
     Chef::Log.info("********** The First deploy::basercms-deploy **********")
     # Setting
-    template "#{deploy[:deploy_to]}/current/app/Config/database.php" do
-      group deploy[:group]
-      owner deploy[:user]
-      mode   '0644'
-      variables({
-        :APP_NAME => app_name
-      })
-      only_if do
-        node[:environment_variables][:db_conf] == 'create'
+    if node[:basercms_deploy][:db_conf] != false
+      template "#{deploy[:deploy_to]}/current/app/Config/database.php" do
+        group deploy[:group]
+        owner deploy[:user]
+        mode   '0644'
+        variables({
+          :APP_NAME => app_name
+        })
       end
     end
   
-    template "#{deploy[:deploy_to]}/current/app/Config/install.php" do
-      group deploy[:group]
-      owner deploy[:user]
-      mode   '0644'
-      variables({
-        :deploy => deploy
-      })
-      only_if do
-        !deploy[:environment_variables][:install].nil? && deploy[:environment_variables][:install] == "create"
+    if !deploy[:environment_variables][:install].nil? && deploy[:environment_variables][:install] == "create"
+      template "#{deploy[:deploy_to]}/current/app/Config/install.php" do
+        group deploy[:group]
+        owner deploy[:user]
+        mode   '0644'
+        variables({
+          :deploy => deploy
+        })
       end
     end
   
