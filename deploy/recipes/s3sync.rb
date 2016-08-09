7a8d09e19ed90373bce0a5954b77299d845865a3
Naoki Iha <n_iha@wan55.co.jp>
2016-08-09 18:25:10 +0900

    Change to sync recipe.

--- deploy/recipes/s3sync.rb
+++ deploy/recipes/s3sync.rb
@@ -19,43 +19,12 @@ node[:deploy].each do |app_name, deploy|
       isCreate = true;
       s3.buckets.create(bucket_name)
     end
-    # Delete Default directory
-    directory "#{deploy[:deploy_to]}/current/app/webroot" do
-      recursive true
-      action :delete
-    end
-    # Create Empty directory
-    directory "#{deploy[:deploy_to]}/current/app/webroot" do
-      user deploy[:user]
-      action :create
-      recursive true
-    end
-    if !::File.exists?("#{deploy[:deploy_to]}/current/app/webroot/index.php")
-      # Mount
-      uid = '4000'#%x(id -u deploy)
-      gid = '48'#%x(id -g apache)
-      execute "s3 Sync by goofys" do
-        command <<-EOH
-          goofys #{bucket_name} #{deploy[:deploy_to]}/current/app/webroot -o allow_other,--uid=#{uid},--gid=#{gid}
-          sleep 30s
-        EOH
-      end
-      # fstab
-      mount "#{deploy[:deploy_to]}/current/app/webroot" do
-        device   "/opt/go/bin/goofys##{bucket_name}"
-        pass     0
-        dump     0
-        fstype   'fuse'
-        options  "_netdev,allow_other,--uid=#{uid},--gid=#{gid}"
-        action   [:mount, :enable]
-      end
-    end
-    if isCreate == true
-      execute "git checkout" do
-        command "git checkout webroot"
-        cwd "#{deploy[:deploy_to]}/current/app"
-        user deploy[:user]
-      end
+    include_recipe 'lsyncd'
+    lsyncd_target 's3' do
+      mode "s3"
+      source "#{deploy[:deploy_to]}/current/app/webroot"
+      target "s3://#{bucket_name}"
+      notifies :restart, 'service[lsyncd]', :delayed
     end
   end
 
