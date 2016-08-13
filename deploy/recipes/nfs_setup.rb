107afa584571152f5390cc191c3b1aaee885afcc
Naoki Iha <sabohoney.com@gmail.com>
2016-08-13 13:02:43 +0900

    Change NFS Server Recipe.

--- deploy/recipes/nfs_setup.rb
+++ deploy/recipes/nfs_setup.rb
@@ -11,6 +11,7 @@ node[:deploy].each do |app_name, deploy|
     writeable true 
     sync true
     options ['no_root_squash']
+    only_if { File.exists?("#{node[:deploy][env[:nfs_app]][:deploy_to]}/current") }
   end
   if !env[:bucket_name].nil? && !env[:bucket_name].empty?
     require 'aws-sdk'
@@ -20,7 +21,7 @@ node[:deploy].each do |app_name, deploy|
       command "aws s3 sync --exact-timestamps s3://#{bucket_name} #{deploy[:deploy_to]}/current"
       user deploy[:user]
       group deploy[:group]
-      only_if { s3.buckets[bucket_name].exists? && s3.buckets[bucket_name].objects['index.php'].exists? }
+      only_if { s3.buckets[bucket_name].exists? && s3.buckets[bucket_name].objects['index.php'].exists? && File.exists?("#{node[:deploy][env[:nfs_app]][:deploy_to]}/current") }
     end
   end
 
