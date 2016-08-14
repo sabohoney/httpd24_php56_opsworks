5ac34fa6dae4037e07ffe307fdfff91848f31144
Naoki Iha <sabohoney.com@gmail.com>
2016-08-15 05:48:03 +0900

    Add recipe for staging.

--- deploy/recipes/s3sync.rb
+++ deploy/recipes/s3sync.rb
@@ -6,6 +6,7 @@
 node[:deploy].each do |app_name, deploy|
 
   env = deploy[:environment_variables]
+  mode = !env[:mode].nil? ? env[:mode] : "none"
   if !env[:bucket_name].nil? && !env[:bucket_name].empty?
     require 'aws-sdk'
     s3 = AWS::S3.new
@@ -16,7 +17,14 @@ node[:deploy].each do |app_name, deploy|
       source "#{deploy[:deploy_to]}/current/app/webroot"
       target "s3://#{bucket_name}"
       notifies :restart, 'service[lsyncd]', :delayed
-      only_if { s3.buckets[bucket_name].exists? && system("mount |grep #{env[:nfs_host]}") }
+      only_if { s3.buckets[bucket_name].exists? && system("mount |grep #{env[:nfs_host]}" && mode.equal?("production")) }
+    end
+    
+    execute "AWS S3 Sync" do
+      command "aws s3 sync --exact-timestamps s3://#{bucket_name} #{deploy[:deploy_to]}/current/app/webroot"
+      user deploy[:user]
+      group deploy[:group]
+      only_if { s3.buckets[bucket_name].exists? && mode.equal?("staging") }
     end
   end
 
