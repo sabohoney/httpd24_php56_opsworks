907842d9b6ee5a43f8726d6f3a46690348336137
Naoki Iha <n_iha@wan55.co.jp>
2016-08-19 22:14:39 +0900

    Execute layer is type 'CMS'

--- deploy/recipes/sync.rb
+++ deploy/recipes/sync.rb
@@ -5,7 +5,7 @@
 
 node[:deploy].each do |application, deploy|
 
-  if deploy[:application_type] != 'php'
+  if deploy[:application_type] != 'php' && node[:opsworks][:instance][:layers].first == 'CMS'
     Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
     next
   end
